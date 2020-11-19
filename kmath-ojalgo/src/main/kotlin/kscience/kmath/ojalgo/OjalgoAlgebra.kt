e4a154d10ab350b8a1e0fd3304fad089882d104c
Iaroslav Postovalov <postovalovya@gmail.com>
2020-11-19 21:32:31 +0700

    Convert exposed Ojalgo scalar algebras to KMath types

--- kmath-ojalgo/src/main/kotlin/kscience/kmath/ojalgo/OjalgoAlgebra.kt
+++ kmath-ojalgo/src/main/kotlin/kscience/kmath/ojalgo/OjalgoAlgebra.kt
@@ -28,3 +28,17 @@ public open class OjalgoField<T, N>(zero: T, one: T) : OjalgoRing<T, N>(zero, on
               T : Operation.Division<T> {
     override fun divide(a: T, b: T): T = a.divide(b)
 }
+
+internal inline fun <T, R> Field<T>.convert(crossinline tToR: (T) -> R, crossinline rToT: (R) -> T): Field<R> =
+    object : Field<R> {
+        override val zero: R
+            get() = tToR(this@convert.zero)
+
+        override val one: R
+            get() = tToR(this@convert.one)
+
+        override fun add(a: R, b: R): R = tToR(this@convert.add(rToT(a), rToT(b)))
+        override fun multiply(a: R, k: Number): R = tToR(this@convert.multiply(rToT(a), k))
+        override fun multiply(a: R, b: R): R = tToR(this@convert.multiply(rToT(a), rToT(b)))
+        override fun divide(a: R, b: R): R = tToR(this@convert.divide(rToT(a), rToT(b)))
+    }
--- kmath-ojalgo/src/main/kotlin/kscience/kmath/ojalgo/OjalgoScalars.kt
+++ kmath-ojalgo/src/main/kotlin/kscience/kmath/ojalgo/OjalgoScalars.kt
@@ -1,15 +1,28 @@
 package kscience.kmath.ojalgo
 
+import kscience.kmath.operations.Complex
+import kscience.kmath.operations.Field
 import org.ojalgo.scalar.*
 
-public val OjalgoMoney = OjalgoField<Scalar<Money>, Money>(Money.ZERO, Money.ONE)
+public val OjalgoMoneyField: Field<Scalar<Money>> = OjalgoField<Scalar<Money>, Money>(Money.ZERO, Money.ONE)
 
-public val OjalgoComplexNumber =
-    OjalgoField<Scalar<ComplexNumber>, ComplexNumber>(ComplexNumber.ZERO, ComplexNumber.ONE)
+public val OjalgoComplexField: Field<Complex> =
+    OjalgoField<Scalar<ComplexNumber>, ComplexNumber>(ComplexNumber.ZERO, ComplexNumber.ONE).convert(
+        { Complex(it.get().real, it.get().imaginary) },
+        { ComplexNumber.of(it.re, it.im) })
 
-public val OjalgoRationalNumber =
-    OjalgoField<Scalar<RationalNumber>, RationalNumber>(RationalNumber.ZERO, RationalNumber.ONE)
+public val OjalgoRationalField: Field<Double> =
+    OjalgoField<Scalar<RationalNumber>, RationalNumber>(
+        RationalNumber.ZERO,
+        RationalNumber.ONE
+    ).convert(Scalar<RationalNumber>::doubleValue, RationalNumber::valueOf)
 
-public val OjalgoQuaternion = OjalgoField<Scalar<Quaternion>, Quaternion>(Quaternion.ZERO, Quaternion.ONE)
+// TODO
+public val OjalgoQuaternionField: Field<Scalar<Quaternion>> =
+    OjalgoField<Scalar<Quaternion>, Quaternion>(Quaternion.ZERO, Quaternion.ONE)
 
-public val OjalgoPrimitiveScalar = OjalgoField<Scalar<Double>, Double>(PrimitiveScalar.ZERO, PrimitiveScalar.ONE)
+public val OjalgoPrimitiveField: Field<Double> =
+    OjalgoField<Scalar<Double>, Double>(PrimitiveScalar.ZERO, PrimitiveScalar.ONE).convert(
+        Scalar<Double>::doubleValue,
+        PrimitiveScalar::valueOf
+    )

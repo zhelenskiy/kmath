9dbd6f2ecfea71f75379d44e03a9c65045b6383b
Iaroslav Postovalov <postovalovya@gmail.com>
2021-02-12 01:14:09 +0700

    Add minimal constant folding to WASM builder

--- kmath-ast/src/jsMain/kotlin/kscience/kmath/wasm/internal/WasmBuilder.kt
+++ kmath-ast/src/jsMain/kotlin/kscience/kmath/wasm/internal/WasmBuilder.kt
@@ -1,35 +1,32 @@
 package kscience.kmath.wasm.internal
 
-import kscience.kmath.internal.webassembly.Instance
 import kscience.kmath.ast.MST
+import kscience.kmath.ast.MST.*
 import kscience.kmath.expressions.Expression
 import kscience.kmath.expressions.StringSymbol
 import kscience.kmath.internal.binaryen.*
-import kscience.kmath.internal.binaryen.ExpressionRef
-import kscience.kmath.internal.binaryen.Type
-import kscience.kmath.internal.binaryen.createType
-import kscience.kmath.internal.binaryen.setOptimizeLevel
+import kscience.kmath.internal.webassembly.Instance
 import kscience.kmath.operations.*
-import kscience.kmath.internal.webassembly.Module as WasmModule
 import kscience.kmath.internal.binaryen.Module as BinaryenModule
+import kscience.kmath.internal.webassembly.Module as WasmModule
 
 private val spreader = eval("(obj, args) => obj(...args)")
 
 @Suppress("UnsafeCastFromDynamic")
 internal sealed class WasmBuilder<T>(
     val binaryenType: Type,
-    val kmathAlgebra: Algebra<T>,
-    val target: MST
+    val algebra: Algebra<T>,
+    val target: MST,
 ) where T : Number {
     val keys: MutableList<String> = mutableListOf()
     lateinit var ctx: BinaryenModule
 
     open fun visitSymbolic(mst: MST.Symbolic): ExpressionRef {
         try {
-            kmathAlgebra.symbol(mst.value)
+            algebra.symbol(mst.value)
         } catch (ignored: Throwable) {
             null
-        }?.let { return visitNumeric(MST.Numeric(it)) }
+        }?.let { return visitNumeric(Numeric(it)) }
 
         var idx = keys.indexOf(mst.value)
 
@@ -41,21 +38,36 @@ internal sealed class WasmBuilder<T>(
         return ctx.local.get(idx, binaryenType)
     }
 
-    abstract fun visitNumeric(mst: MST.Numeric): ExpressionRef
+    abstract fun visitNumeric(mst: Numeric): ExpressionRef
 
-    open fun visitUnary(mst: MST.Unary): ExpressionRef =
+    open fun visitUnary(mst: Unary): ExpressionRef =
         error("Unary operation ${mst.operation} not defined in $this")
 
-    open fun visitBinary(mst: MST.Binary): ExpressionRef =
+    open fun visitBinary(mst: Binary): ExpressionRef =
         error("Binary operation ${mst.operation} not defined in $this")
 
     open fun createModule(): BinaryenModule = js("new \$module\$binaryen.Module()")
 
     fun visit(mst: MST): ExpressionRef = when (mst) {
-        is MST.Symbolic -> visitSymbolic(mst)
-        is MST.Numeric -> visitNumeric(mst)
-        is MST.Unary -> visitUnary(mst)
-        is MST.Binary -> visitBinary(mst)
+        is Symbolic -> visitSymbolic(mst)
+        is Numeric -> visitNumeric(mst)
+
+        is Unary -> when {
+            algebra is NumericAlgebra && mst.value is Numeric -> visitNumeric(
+                Numeric(algebra.unaryOperationFunction(mst.operation)(algebra.number(mst.value.value))))
+
+            else -> visitUnary(mst)
+        }
+
+        is Binary -> when {
+            algebra is NumericAlgebra && mst.left is Numeric && mst.right is Numeric -> visitNumeric(Numeric(
+                algebra
+                    .binaryOperationFunction(mst.operation)
+                    .invoke(algebra.number(mst.left.value), algebra.number(mst.right.value))
+            ))
+
+            else -> visitBinary(mst)
+        }
     }
 
     val instance by lazy {
@@ -93,9 +105,9 @@ internal sealed class WasmBuilder<T>(
 internal class RealWasmBuilder(target: MST) : WasmBuilder<Double>(f64, RealField, target) {
     override fun createModule(): BinaryenModule = readBinary(f64StandardFunctions)
 
-    override fun visitNumeric(mst: MST.Numeric): ExpressionRef = ctx.f64.const(mst.value)
+    override fun visitNumeric(mst: Numeric): ExpressionRef = ctx.f64.const(mst.value)
 
-    override fun visitUnary(mst: MST.Unary): ExpressionRef = when (mst.operation) {
+    override fun visitUnary(mst: Unary): ExpressionRef = when (mst.operation) {
         SpaceOperations.MINUS_OPERATION -> ctx.f64.neg(visit(mst.value))
         SpaceOperations.PLUS_OPERATION -> visit(mst.value)
         PowerOperations.SQRT_OPERATION -> ctx.f64.sqrt(visit(mst.value))
@@ -116,7 +128,7 @@ internal class RealWasmBuilder(target: MST) : WasmBuilder<Double>(f64, RealField
         else -> super.visitUnary(mst)
     }
 
-    override fun visitBinary(mst: MST.Binary): ExpressionRef = when (mst.operation) {
+    override fun visitBinary(mst: Binary): ExpressionRef = when (mst.operation) {
         SpaceOperations.PLUS_OPERATION -> ctx.f64.add(visit(mst.left), visit(mst.right))
         SpaceOperations.MINUS_OPERATION -> ctx.f64.sub(visit(mst.left), visit(mst.right))
         RingOperations.TIMES_OPERATION -> ctx.f64.mul(visit(mst.left), visit(mst.right))
@@ -127,15 +139,15 @@ internal class RealWasmBuilder(target: MST) : WasmBuilder<Double>(f64, RealField
 }
 
 internal class IntWasmBuilder(target: MST) : WasmBuilder<Int>(i32, IntRing, target) {
-    override fun visitNumeric(mst: MST.Numeric): ExpressionRef = ctx.i32.const(mst.value)
+    override fun visitNumeric(mst: Numeric): ExpressionRef = ctx.i32.const(mst.value)
 
-    override fun visitUnary(mst: MST.Unary): ExpressionRef = when (mst.operation) {
+    override fun visitUnary(mst: Unary): ExpressionRef = when (mst.operation) {
         SpaceOperations.MINUS_OPERATION -> ctx.i32.sub(ctx.i32.const(0), visit(mst.value))
         SpaceOperations.PLUS_OPERATION -> visit(mst.value)
         else -> super.visitUnary(mst)
     }
 
-    override fun visitBinary(mst: MST.Binary): ExpressionRef = when (mst.operation) {
+    override fun visitBinary(mst: Binary): ExpressionRef = when (mst.operation) {
         SpaceOperations.PLUS_OPERATION -> ctx.i32.add(visit(mst.left), visit(mst.right))
         SpaceOperations.MINUS_OPERATION -> ctx.i32.sub(visit(mst.left), visit(mst.right))
         RingOperations.TIMES_OPERATION -> ctx.i32.mul(visit(mst.left), visit(mst.right))
